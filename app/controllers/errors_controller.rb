3507e2220e954ca5eff78d11d64c9cf66aac8b35
Jordan Taylor <football80@gmail.com>
2021-11-29 21:16:33 -0800

    Hopefully fixing error emails

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,15 +1,15 @@
 class ErrorsController < ApplicationController
 	def show
-		@url = request.original_url
+		url = request.original_url
 		# If i want to just email the normal rails error page, do this:
 		# https://www.honeybadger.io/blog/how-rails-fancy-exception-page-works/
-		@exception = request.env["action_dispatch.exception"]
-		exception_wrapper = ActionDispatch::ExceptionWrapper.new(request.env['action_dispatch.backtrace_cleaner'], @exception)
+		exception = request.env["action_dispatch.exception"]
+		exception_wrapper = ActionDispatch::ExceptionWrapper.new(request.env['action_dispatch.backtrace_cleaner'], exception)
 		trace = exception_wrapper.application_trace
 
 		render action: request.path[1..-1]
 		if params[:status] != "404"
-			ErrorMailer.error_details(@url, @exception, exception_wrapper, trace).deliver_now
+			ErrorMailer.error_details(url, exception, exception_wrapper, trace).deliver_now
 		end
 		# email = ErrorMailer.error_details(@exception).deliver_now
 		# email.delivery_method.settings.merge!({user_name: ENV['GMAIL_WEBMASTER_ID'], password: ENV['GMAIL_WEBMASTER_PASSWORD']}) #for using non default email
