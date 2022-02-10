76e80ca88def322e7ae266794662a6c698355bb5
Jordan Taylor <football80@gmail.com>
2022-02-09 23:43:41 -0800

    Updated errors accross the site. Hopefully error messages look much better now and don't mess up the styling of forms anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
 	has_secure_password
 	
 	validates_presence_of :email
-  validates :password, :presence => true, :confirmation => true, :on => :create
 	validates_uniqueness_of :email
+  validates :password, :presence => true, :confirmation => true, :on => :create
 
 	def self.search(search)
   		if search
--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,12 @@ load(app_environment_variables) if File.exists?(app_environment_variables)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# no error div on labels
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  unless html_tag =~ /^<label/
+    "<div class=\"field_with_errors\">#{html_tag}</div>".html_safe
+  else
+    html_tag.html_safe
+  end
+end
