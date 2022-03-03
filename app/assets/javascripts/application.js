7c8424581b9ccffb12c5fc8d4e71ceb67b057cb1
Jordan Taylor <football80@gmail.com>
2022-03-03 00:12:52 -0800

    Ok for real, the resizing function works now. I was over complicating it with the bisect thing which doesn't work because the text height would snap back and forth if the resize caused a line break to occur

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,6 @@
 
 /*global $*/
 
-console.warn("Loading application.js");
 $(function() { $(document).foundation(); });
 
 $(window).on("load", function() {
@@ -194,76 +193,76 @@ $(function() {
 });
 
 // Handle auto text resizing:
-const autoSizeText = function() {
+const autoSizeText = async function() {
   const elements = $('.resize');
   if (elements.length <= 0) {
+    console.log("No elements found that need resizing.");
     return;
   }
 
   // 0.7 is used because that is the ratio of the squares diagonal to its side. And since we need the square to
   // fit inside the bubble (whose diameter is the same as the squares diagonal) we have to scale our scroll area
-  // by this ratio. BUT because words are funny, and they do weird layouts, we grow this a bit to try and fill
-  // the circle with text. At 0.7 it is GUARANTEED all text will be in the bubble, but a lot of the time it looks
-  // like it is just too small.
-  const magic_scaling_number = 0.7 + 0.05;
-  let results = [];
+  // by this ratio.
+  const magic_scaling_number = 0.7;
 
   // Loop over all elements that have requested a text resize
   for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
-    results.push((function(element) {
-      let min_font_size = 20;
-      let max_font_size = parseInt($(element).data("maxFontSize"));
+    
+    // .resize elements have a starting font-size of 0
+    let current_test_size = 30;
 
-      // .resize elements have a starting font-size of 0
-      let current_test_size = min_font_size;
+    const max_size = element.scrollWidth * magic_scaling_number;
+    const text_box = element.children[0];
 
-      const max_size = element.scrollWidth * magic_scaling_number;
+    // add margins to the side that fill in based on the magic_scaling_number
+    $(text_box).css('margin', "0 " + ((1 - magic_scaling_number) / 2) * 100 + "%");
 
-      const resizeText = function() {
-        const text_box = element.children[0];
-        // console.log("scrollWidth: " + text_box.scrollWidth + " | clientWidth: " + text_box.clientWidth + " | offsetWidth: " + text_box.offsetWidth);
-        // console.log("scrollHeight: " + text_box.scrollHeight + " | clientHeight: " + text_box.clientHeight + " | offsetWidth: " + text_box.offsetHeight);
-        const is_overflowing =
-            text_box.scrollWidth > text_box.clientWidth || // verifies no horizontal overflow
-            text_box.clientWidth > max_size || // verifies that the full width fits in the bubble
-            text_box.scrollHeight > text_box.clientHeight || // verifies no vertical overflow (probably has no impact on this scenario)
-            text_box.clientHeight > max_size; // verifies height does not exceen the bubble
-            
-        if (is_overflowing) {
-          // text is out of bounds, go half the distance back towards previous value (min_font_size)
-          max_font_size = current_test_size;
-          current_test_size = current_test_size + (min_font_size - current_test_size) / 2;
-        } else {
-          // text doesn't go out of bounds yet, add half the amount towards max and current value and try again
-          min_font_size = current_test_size;
-          current_test_size = current_test_size + (max_font_size - current_test_size) / 2;
-        }
-        // add margins to the side that fill in based on the magic_scaling_number
-        $(text_box).css('margin', "0 " + ((1 - magic_scaling_number) / 2) * 100 + "%");
-        $(element).css('font-size', current_test_size);
-      };
+    let has_overflowed = false;
+    const resizeText = function() {
+      // Set font size based on current test size (determined from previous loops test sizes)
+      $(element).css('font-size', current_test_size + "px");
+      console.log("testing font size: " + current_test_size);
 
-      // uncomment this to be able to watch how it resizes
-      // setInterval(() => {
-      //   if (Math.abs(max_font_size - min_font_size > 1)) {
-      //     resizeText();
-      //   }
-      // }, 500)
+      // console.log("maxSize: " + max_size + " | scrollWidth: " + text_box.scrollWidth + " | clientWidth: " + text_box.clientWidth + " | offsetWidth: " + text_box.offsetWidth);
+      // console.log("maxSize: " + max_size + " | scrollHeight: " + text_box.scrollHeight + " | clientHeight: " + text_box.clientHeight + " | offsetHeight: " + text_box.offsetHeight);
+      const is_overflowing =
+          text_box.scrollWidth > text_box.clientWidth ||    // verifies no horizontal overflow
+          text_box.clientWidth > max_size ||                // verifies that the full width fits in the bubble
+          text_box.scrollHeight > text_box.clientHeight ||  // verifies no vertical overflow (probably has no impact on this scenario)
+          text_box.clientHeight > max_size;                 // verifies height does not exceed the bubble
+          
+      // console.log("min: " + min_font_size + " | max: " + max_font_size + " | current: " + current_test_size);
 
-      // if the gap between min and max is too big, resize again.
-      while (Math.abs(max_font_size - min_font_size) > 1) {
-          resizeText();
+      // The idea here is to scale up until we overflow, then scale down until we are not overflowing anymore.
+      if (is_overflowing) {
+        // text is out of bounds, shrink by 0.5
+        current_test_size = current_test_size - 0.5;
+        has_overflowed = true;
+      } else if (!has_overflowed) {
+        // text doesn't go out of bounds yet, add 10
+        current_test_size = current_test_size + 5;
+      } else {
+        console.warn("Decided on font size: " + current_test_size)
+        return;
       }
-    })(element));
+      if (max_size > 0){
+        resizeText();
+      }
+    };
+
+    resizeText();
   }
-  return results;
 };
 
 $(document).ready(function() {
-  return autoSizeText();
+  autoSizeText();
 });
 
+let resize_timeout;
 $(window).resize(function() {
-  return autoSizeText();
+  clearTimeout(resize_timeout);
+  resize_timeout = setTimeout(() => {
+    autoSizeText();
+  }, 500);
 });
