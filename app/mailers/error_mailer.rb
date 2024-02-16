2150b07f45c871ef59365b7dc3934e4a9c21dea8
Jordan Taylor <football80@gmail.com>
2024-02-16 02:39:54 -0800

    Fixing error mailer

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -1,11 +1,14 @@
-def error_details(url, error, exception_wrapper, trace, request_params = nil, user = nil)
-  @url = url
-  @error = error
-  @exception_wrapper = exception_wrapper
-  @trace = trace
-  @request_params = request_params
-  @user = user # Assuming you have user information
-  @message = error.nil? ? "No Error" : error.message
-  @timestamp = Time.current.to_s(:db) # Format as you prefer
-  mail to: "trickswebmaster@gmail.com", subject: "Website Error: #{@message}"
+class ErrorMailer < ApplicationMailer
+  def error_details(url, error, exception_wrapper, trace, request_params = nil, user = nil)
+    @url = url
+    @error = error
+    @exception_wrapper = exception_wrapper
+    @trace = trace
+    @request_params = request_params
+    @user = user # Assuming you have user information
+    @message = error.nil? ? "No Error" : error.message
+    @timestamp = Time.current.to_s(:db) # Format as you prefer
+    mail to: "trickswebmaster@gmail.com", subject: "Website Error: #{@message}"
+  end
+
 end
