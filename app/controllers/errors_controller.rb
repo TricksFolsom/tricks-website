79f1432b3fd438e2327a36f4b376102857819136
Jordan Taylor <football80@gmail.com>
2024-02-16 02:36:13 -0800

    Updated error email and created problem in comments controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,19 +1,21 @@
 class ErrorsController < ApplicationController
-	def show
-		url = request.original_url
-		# If i want to just email the normal rails error page, do this:
-		# https://www.honeybadger.io/blog/how-rails-fancy-exception-page-works/
-		exception = request.env["action_dispatch.exception"]
-		exception_wrapper = ActionDispatch::ExceptionWrapper.new(request.env['action_dispatch.backtrace_cleaner'], exception)
-		trace = exception_wrapper.application_trace
+  def show
+    url = request.original_url
+    exception = request.env["action_dispatch.exception"]
+    exception_wrapper = ActionDispatch::ExceptionWrapper.new(request.env['action_dispatch.backtrace_cleaner'], exception)
+    trace = exception_wrapper.application_trace
+    request_params = request.filtered_parameters # Ensure this only includes what you're comfortable logging
+    user_info = current_user.try(:attributes) # Adjust based on your user model, ensure sensitive info is excluded
 
-		render action: request.path[1..-1]
-		if params[:status] != "404"
-			ErrorMailer.error_details(url, exception, exception_wrapper, trace).deliver_now
-		end
-		
-		# email = ErrorMailer.error_details(@exception).deliver_now
-		# email.delivery_method.settings.merge!({user_name: ENV['GMAIL_WEBMASTER_ID'], password: ENV['GMAIL_WEBMASTER_PASSWORD']}) #for using non default email
-		# email.deliver_now
-	end  
+    # Optionally, only email for certain types of errors
+    if params[:status] != "404"
+      ErrorMailer.error_details(url, exception, exception_wrapper, trace, request_params, user_info).deliver_now
+    end
+
+    # Attempt to render the error page if one exists for the status code
+    render action: request.path[1..-1], status: params[:status] || 500
+  rescue ActionController::UnknownFormat, ActionView::MissingTemplate => e
+    # If there's no template for the error, you might want to render a generic error page
+    render "errors/generic_error", status: params[:status] || 500 # Ensure you have a generic_error.html.erb
+  end
 end
