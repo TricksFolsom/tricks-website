a2f56284b2691d9fc6bec4a4806aff909444c753
Jordan Taylor <football80@gmail.com>
2024-04-24 00:23:49 -0700

    Loosening restrictions on creating an employment application, also re-enabled failure emails

--- app/controllers/employment_applications_controller.rb
+++ app/controllers/employment_applications_controller.rb
@@ -108,16 +108,16 @@ class EmploymentApplicationsController < ApplicationController
 
   # POST /employment_applications
   def create
-    unless verify_recaptcha(model: @employment_application, action: 'submit', minimum_score: 0.7)
-      # str = "Recaptcha or priorities failed for: #{params[:employment_application][:firstname]} #{params[:employment_application][:lastname]} | #{params[:employment_application][:email]}"
-      # CommentMailer.comment_notification(str).deliver_now
+    unless verify_recaptcha(model: @employment_application, action: 'submit', minimum_score: 0.5)
+      str = "Recaptcha failed for: #{params[:employment_application][:firstname]} #{params[:employment_application][:lastname]} | #{params[:employment_application][:email]}"
+      CommentMailer.comment_notification(str).deliver_now
       redirect_to root_path
       return
     end
 
     if params[:employment_application][:honeypot].present?
-      # str = params[:employment_application][:firstname] + " " + params[:employment_application][:lastname] + " | " + params[:employment_application][:email] + " | " + params[:employment_application][:honeypot]
-      # CommentMailer.comment_notification(str).deliver_now
+      str = "Honey Pot Caught this one:" + params[:employment_application][:firstname] + " " + params[:employment_application][:lastname] + " | " + params[:employment_application][:email] + " | " + params[:employment_application][:honeypot]
+      CommentMailer.comment_notification(str).deliver_now
       redirect_to root_path
       return
     end
